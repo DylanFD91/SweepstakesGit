04642672d5c93d72c420f2ba5d56ae1c7db0fa26
Dylan <dylan.f.derouchie@gmail.com>
2020-03-02 15:13:03 -0600

    completed the sweepstakes queue and stack classes, moving towards the marketing firm now.

--- SweepStakes/SweepstakesQueueManager.cs
+++ SweepStakes/SweepstakesQueueManager.cs
@@ -6,12 +6,12 @@ using System.Threading.Tasks;
 
 namespace SweepStakes
 {
-    class SweepstakesStackManager : ISweepstakesManager
+    class SweepstakesQueueManager : ISweepstakesManager
     {
         //Members
 
         //Constructor
-        public SweepstakesStackManager()
+        public SweepstakesQueueManager()
         {
 
         }
