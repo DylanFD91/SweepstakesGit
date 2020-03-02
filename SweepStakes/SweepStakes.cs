7fa532cb2c6df4a55148f853be1f684a967fb4b5
Dylan <dylan.f.derouchie@gmail.com>
2020-03-02 10:27:29 -0600

    Made initial classes to start the barebones

---
+++ SweepStakes/SweepStakes.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SweepStakes
+{
+    class SweepStakes
+    {
+        //Members
+
+        //Constructor
+
+        //Methods
+        public void Sweepstakes(string name)
+        {
+
+        }
+        public void RegisterContestant(Contestant contestant)
+        {
+
+        }
+        public void PickWinner()
+        {
+
+        }
+        public void PrintContestantInfo(Contestant contestant)
+        {
+
+        }
+    }
+}
